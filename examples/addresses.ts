6f0409628643bed00e35459272845a90127a548f
Anton Cheng <antonassocareer@gmail.com>
2023-11-23 14:10:00 +0800

    chore: withdrawal

--- examples/addresses.ts
+++ examples/addresses.ts
@@ -9,5 +9,17 @@ export const addresses = {
     usdc: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
     fastConnector: '0x200AF8FCdD5246D70B369A98143Ac8930A077B7A',
     nativeConnector: '0x22d8360eB04F46195c7B02A66658C375948d8A99'
+  },
+  lyraTestnet:{
+    usdc: '0xe80F2a02398BBf1ab2C9cc52caD1978159c215BD',
+    withdrawalHelper: '0x621b3519132B3b90DeBFCC059a9c0386C91d5723',
+    fastConnector: '0x03027410F25c527D5aEB3E6e56a6389611DCb2A9',
+    nativeConnector: '0xA7f7e4fE8E4cdDCD9969Bd3fbcFF67000CD7DE47'
+  },
+  lyra: {
+    usdc: '0x6879287835A86F50f784313dBEd5E5cCC5bb8481',
+    withdrawalHelper: '0x93fa965d5584e03f8087D4b6a485D610356a3dea', // todo: update to new version with viewer
+    fastConnector: '0x1281C1464449DB73bdAa30928BCC63Dc25D8D187',
+    nativeConnector: '0x19609EeE157BC38E1A40E19c38218fC3900d97ac'
   }
 }
---
+++ examples/l2/withdraw-fast-connector.ts
@@ -0,0 +1,57 @@
+import { BigNumber, constants, ethers, utils } from 'ethers'
+import { addresses } from '../addresses';
+import { signReceiveWithAuth } from "../sigUtils"
+import withdrawalHelperAbi from "../abi/withdraw-helper.json";
+import usdcAbi from "../abi/usdc.json";
+
+// dot env
+import dotenv from 'dotenv'
+dotenv.config()
+
+const RPC_URL = process.env.LYRA_TESTNET_RPC!
+
+const OWNER_PK = process.env.OWNER_PRIVATE_KEY!
+
+const networkConfig = addresses.lyraTestnet
+
+// This will be connected wallet in Metamask
+const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
+const user = new ethers.Wallet(OWNER_PK, provider);
+
+
+// contract instances
+const helper = new ethers.Contract(networkConfig.withdrawalHelper, withdrawalHelperAbi, user);
+
+/**
+ * npx ts-node examples/l2/withdraw-fast-connector.ts
+ * 
+ */
+async function run() {
+  console.log('Withdraw USDC from Lyra Chain', user.address)
+  console.log('NetworkID:\t', (await (provider.getNetwork())).chainId)
+
+
+  const usdc = new ethers.Contract(networkConfig.usdc, usdcAbi, user.provider)
+  const balance = await usdc.balanceOf(user.address)
+  console.log('USDC Balance:\t', utils.formatUnits(balance, 6), 'USDC')
+
+  const amount = '10000000';
+
+  // whole tx
+  const minGas = '500000'
+
+  const feeInUSDC = await helper.getFeeUSDC(networkConfig.fastConnector, minGas)
+  console.log('Fee in USDC:\t', utils.formatUnits(feeInUSDC, 6), 'USDC')
+  
+  // these txs should be batched
+
+  // 1. approve
+  const tx1 = await usdc.connect(user).approve(networkConfig.withdrawalHelper, balance)
+  console.log('Approving...', tx1.hash)
+  // 2. withdraw
+  const tx = await helper.connect(user).withdrawToL1(amount, user.address, networkConfig.fastConnector, minGas)
+  console.log('Withdrawing...', tx.hash)
+}
+
+
+run();
